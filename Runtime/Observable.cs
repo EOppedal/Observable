6c4606f0a467600477403f9f07ea6f14bbb7e638
EOppedal <erlend.oppedal@gmail.com>
2024-09-02 20:19:57 +0200

    init

---
+++ Runtime/Observable.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Holds a reference to a variable if type T and an event that gives the value to the variable when changed
+/// </summary>
+[Serializable] public class Observable<T> {
+    public event UnityAction<T> OnValueChanged = delegate { };
+
+    [SerializeField] private T value;
+
+    public T Value {
+        get => value;
+        set {
+            // if (EqualityComparer<T>.Default.Equals(this.value, value)) return;
+            this.value = value;
+            OnValueChanged?.Invoke(value);
+        }
+    }
+
+    public Observable(T value = default) => this.value = value;
+
+    public void ForceUpdate() {
+        OnValueChanged?.Invoke(Value);
+    }
+}
